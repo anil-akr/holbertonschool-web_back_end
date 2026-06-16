b71c606aa2ffc9f3a02be5284a9b9c483d05b014
anil-akr <4nil000@protonmail.com>
2026-06-16 15:04:55 +0200

    T0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Async generator that yields random numbers between 0 and 10"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Loop 10 times, wait 1 second each time and yield
+    a random float between 0 and 10"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
