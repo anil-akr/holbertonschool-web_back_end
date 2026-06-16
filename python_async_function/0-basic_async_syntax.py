ea88a5e756846de54912d6a2b88d55b1059c99de
anil-akr <4nil000@protonmail.com>
2026-06-16 10:17:35 +0200

    T3

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,11 +4,12 @@
 named wait_random that waits for a random delay between 0 and max_delay
 (included and float value) seconds and eventually returns it."""
 import random
-import asyncio 
+import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Wait for a random delay between 0 and max_delay seconds and return it."""
+    """Wait for a random delay between 0 and max_delay
+    seconds and return it."""
 
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Create an asynchio Task from the wait_random coroutine"""
+import asyncio
+
+
+def task_wait_random(max_delay: int):
+    """Return an asynchio Task that runs wait_random with max_delay"""
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    task = asyncio.get_event_loop().create_task(wait_random(max_delay))
+    return task
