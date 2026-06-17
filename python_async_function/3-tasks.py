7731819ac400f4a2582f48ae8c729ced9c02f4eb
anil-akr <4nil000@protonmail.com>
2026-06-17 09:37:42 +0200

    Fix T3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -3,7 +3,7 @@
 import asyncio
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Return an asynchio Task that runs wait_random with max_delay"""
     wait_random = __import__('0-basic_async_syntax').wait_random
     task = asyncio.get_running_loop().create_task(wait_random(max_delay))
