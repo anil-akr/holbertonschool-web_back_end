a1bf72cf9b9701e6f3465d812f8209a84998b5d3
anil-akr <4nil000@protonmail.com>
2026-06-17 09:34:11 +0200

    Change get event loop to get running loop

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,5 +6,5 @@ import asyncio
 def task_wait_random(max_delay: int):
     """Return an asynchio Task that runs wait_random with max_delay"""
     wait_random = __import__('0-basic_async_syntax').wait_random
-    task = asyncio.get_event_loop().create_task(wait_random(max_delay))
+    task = asyncio.get_running_loop().create_task(wait_random(max_delay))
     return task
